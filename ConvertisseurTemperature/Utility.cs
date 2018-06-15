05cd73841670dbad3d9985a2c7475ef298d57721
hami kaveii <hamidreza.kaveii@gmail.com>
2018-06-15 00:15:15 -0400

    final

---
+++ ConvertisseurTemperature/Utility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_BasicProject
+{
+    class Utility
+    {
+
+        public static string cversf(string textCel)
+        {
+            int userCel = int.Parse(textCel);
+            int tempFar = (userCel * 2) + 32;
+            string textFar = tempFar.ToString();
+            return textFar;
+        }
+
+        public static string fversc(string textFar)
+        {
+            int userFar = int.Parse(textFar);
+            int tempCel = (userFar - 32) / 2;
+            string textCel = tempCel.ToString();
+            return textCel;
+
+        }
+    }
+}
