990a0076565fe239bb0a5d02b09913a196341de5
hami kaveii <hamidreza.kaveii@gmail.com>
2018-06-16 14:04:31 -0400

    Demo2 changes

---
+++ Demo1_first_project/ProjetDebutant/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_BasicProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectionImage_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Charger l image dans l`espace réservé
+                pictureImage.Image = Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+        private void quitterImage_Click(object sender, EventArgs e)
+        {
+            //quitter l application
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
