57ff776452ba41042cd520775665d76e39cb624d
hami kaveii <hamidreza.kaveii@gmail.com>
2018-06-16 13:37:10 -0400

    Demo_type

---
+++ Demo_type/Exe1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrer ton nom:");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Votre nom est:{0}" , nom);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Demo_type/Exe7/Facturette.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe7
+{
+    //class Facturette
+    //{
+        struct Facturette
+        {
+            int no;
+            int nombre;
+            string nom;
+            double prix;
+            public Facturette(int no, int nombre, string nom, double prix)
+            {
+                this.no = no;
+                this.nombre = nombre;
+                this.nom = nom;
+                this.prix = prix;
+            }
+            public string ToString() {
+
+            }  // methodes
+        }
+    //}
+}
