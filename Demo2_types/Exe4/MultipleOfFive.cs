742ec4d6c299af9fec5e7bce70bf83696fab71a2
1795545 <1795545@LI-S068-0T26>
2018-06-19 11:37:24 -0400

    Exe4 creation

---
+++ Demo2_types/Exe4/MultipleOfFive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe4
+{
+    class MultipleOfFive
+    {
+
+        public static void MultiplierDeCinq(int nombre)
+        {
+            Console.WriteLine("Les {0} premiers multiples de 5 est:", nombre);
+            for (int i = 1; i <= nombre; i++)
+            {
+                Console.Write(i * 5 + " ");
+            }
+        }
+    }
+}
---
+++ Demo2_types/Exe4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Variable declaration
+            int nombre;
+
+            //Print the message for user
+            Console.WriteLine("Entrer le nombre: ");
+
+            //Check the user entry
+            while(!(int.TryParse(Console.ReadLine(), out nombre)) || nombre<=0)
+            {
+                Console.WriteLine("Errour de saisi, entrer le nombre: ");
+            }
+
+            //Run the methode for show the multiple of 5
+            MultipleOfFive.MultiplierDeCinq(nombre);
+
+            //Wait for the key before close console
+            Console.ReadKey();
+        }
+    }
+}
