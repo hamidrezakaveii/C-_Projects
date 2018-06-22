192e521534187b70c0de4ef820f5c89adf121abc
Hami Kaveii <hamidreza.kaveii@gmail.com>
2018-06-22 14:48:57 -0400

    Demo3

---
+++ Demo3_Classes/Exe1/PanierAchat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe1
+{
+    class PanierAchat
+    {   
+        //definition de varibles
+        private string _noItem;
+        private string _nomItem;
+        private string _tailleItem;
+        private decimal _prixItem;
+
+        //definition de proprieté
+        public string NoItem { get{ return _noItem; } set { _noItem = value; } }
+        public string NomItem { get { return _nomItem; } set{ _nomItem = value; } }
+        public string TailleItem { get { return _tailleItem; } set { if (value.Equals("0")) { TailleItem = "taille inconnue"; } else { _tailleItem = value; } } }
+        public decimal PrixItem { get { return _prixItem; } set { if (value < 0) PrixItem = 0; else _prixItem = value; } }
+
+        //definition de constructor 
+        public PanierAchat(string no, string nom, string taille, decimal prix)
+        {
+            NoItem = nom;
+            NomItem = nom;
+            TailleItem = taille;
+            PrixItem = prix;
+        }
+
+        //override methode ToString()
+        public override string ToString()
+        {
+
+            Console.WriteLine("Item#: " + NomItem + " \nDescription: " + NomItem + " \nTaille: " + TailleItem + " \nPrix unitaire: " + PrixItem +" $");
+            return "";
+        }
+        
+    }
+}
+
---
+++ Demo3_Classes/Exe1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //definition de variable
+            string no;
+            string nom;
+            string taille;
+            decimal prix;
+            
+            //afficher message pour initialiser des variable
+            Console.WriteLine("Entrer le code article");
+            no = Console.ReadLine();
+            Console.WriteLine("Entrer le nom article");
+            nom = Console.ReadLine();
+            Console.WriteLine("Entrer la taille article");
+            taille = Console.ReadLine();
+            Console.WriteLine("Entrer le prix article");
+            prix = Convert.ToDecimal(Console.ReadLine());
+
+            //creeration de l'objet
+            PanierAchat monPanier = new PanierAchat(no,nom,taille,prix);
+
+            //afficher le resultat
+            Console.WriteLine(monPanier);
+
+            //atendre pour le key avant exit le console
+            Console.ReadKey();
+        }
+    }
+}
