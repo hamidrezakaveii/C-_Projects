1721ad79ce4078d7430973e751b93d32ee8a6d50
hami kaveii <hamidreza.kaveii@gmail.com>
2018-07-02 17:54:54 -0400

    delegate changes

---
+++ Demo3_Delegates/Manipulation N2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulation_N2
+{
+    //delegate creation
+    delegate double delegateAdvance(double valA, int valB);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate delegate
+            delegateAdvance myDelegate = new delegateAdvance(methodeDelegateProduct);
+
+            //invoke delegate
+            Console.WriteLine("the result is: {0:f2}",myDelegate(10.0, 4));
+            Console.ReadKey();
+        }
+
+        private static double methodeDelegateProduct(double val1, int val2)
+        {
+            Console.WriteLine("You are using a delegate!");
+            return val1 * val2;
+        }
+    }
+}
---
+++ Demo3_Delegates/Manipulation N3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulation_N3
+{
+    //delegate creation 
+    delegate double delegateParam(double valA, int valB);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            delegateParam myDelegate = new delegateParam(methodForDelegate);
+            methodService(myDelegate, "Hami");
+            Console.ReadKey();
+
+        }
+
+        private static void methodService(delegateParam p, string txt)
+        {
+            Console.WriteLine(txt+" The result is: {0:f2}",p(10.0, 4));
+        }
+
+        private static double methodForDelegate(double val1, int val2)
+        {
+            Console.WriteLine("You are try to use a delegate!");
+            return val1 * val2;
+        }
+    }
+}
