a68d504a0f0341cab392308c5f87e8dc9f72619c
Hami Kaveii <hamidreza.kaveii@gmail.com>
2018-08-21 17:07:37 -0400

    add project

---
+++ boutonuser-control_2015/test_bouton_2015/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test_bouton_2015
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void userBouton1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bouton a été cliqué!");
+        }
+
+        private void Afficher_Click(object sender, EventArgs e)
+        {
+            userBouton1.Musure = Convert.ToInt32(textBox1.Text);
+        }
+
+        private void userBouton1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
