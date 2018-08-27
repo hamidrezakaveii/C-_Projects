8f3125df5592fd9b05961b2bc81076b21dbf93be
Hami Kaveii <hamidreza.kaveii@gmail.com>
2018-08-27 13:36:22 -0400

    add pr

---
+++ Demo7_heritage/Dem7_Heritage/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dem7_Heritage
+{
+    class Program
+    {
+
+        class A
+        {
+            public virtual void MonCalcul()
+            {
+
+            }
+
+            public static void MonToto()
+            {
+                Console.WriteLine("Base");
+            }
+        }
+
+        class B : A
+        {
+            public override void MonCalcul()
+            {
+                base.MonCalcul();
+            }
+
+            public static void MonToto()
+            {
+                Console.WriteLine("Herité");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            B.MonToto();
+        }
+    }
+}
