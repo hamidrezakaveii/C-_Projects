08622087b6828a31c42d9bb50e01633bf047dd2c
Hami Kaveii <hamidreza.kaveii@gmail.com>
2018-08-28 16:34:12 -0400

    Changes

--- Demo8_ADOMySql/Demo8_ADOMySql/Program.cs
+++ Demo8_ADOMySql/Demo8_ADOMySql/Program.cs
@@ -15,32 +15,103 @@ namespace Demo8_ADOMySql
             string cs = "server=localhost;user=root;" +
                 "database=northwindmysql;port=3306;";
             //objet connexion
-            MySqlConnection connection = new MySqlConnection(cs);
+            MySqlConnection connection = null;
+            MySqlDataReader reader = null;
 
             try
             {
                 
                 Console.WriteLine("Connection sur MySql...");
+                connection = new MySqlConnection(cs);
                 connection.Open();
 
+                ////creer une requete
+                //string query = "SELECT ContactName, CompanyName from customers";
+
+                ////Preparer L'execution de la requte
+                //MySqlCommand sql = new MySqlCommand(query, connection);
+
+                ////Recuperer le cursur
+                // reader = sql.ExecuteReader();
+
+                ////Parcourir le curseur
+                //while (reader.Read())
+                //{
+                //    //afficher detail
+                //    string nom, company;
+                //    nom = (string)reader[0];
+                //    company = (string)reader[1];
+                //    Console.WriteLine("nom: {0} - compagnie: {1}", nom, company);
+                //}
+
+                //insert 
+                //string queryinsert = "INSERT INTO categories(CategoryName, Description, Picture) values ('sport', 'Tous les articles de sport', 'sport.gif')";
+                //MySqlCommand sql2 = new MySqlCommand(queryinsert, connection);
+                //sql2.ExecuteNonQuery();
+
+                //update
+                //string queryupdate = "UPDATE categories set Description ='Tous les articles de sport 2019' WHERE CategoryName='sport'";
+                //MySqlCommand sql3 = new MySqlCommand(queryupdate, connection);
+                //sql3.ExecuteNonQuery();
+
+                //delete
+                //string querydelete = "DELETE FROM categories WHERE CategoryName='sport'";
+                //MySqlCommand sql4 = new MySqlCommand(querydelete, connection);
+                //sql4.ExecuteNonQuery();
+
+                //Opérations d’agrégation
+                //string querymax = "SELECT max(UnitPrice) from products";
+                //MySqlCommand sql5 = new MySqlCommand(querymax, connection);
+                //double max = (double)sql5.ExecuteScalar();
+                //Console.WriteLine(max);
+
+                //Opérations d’agrégation
+                //string querymin = "SELECT min(UnitPrice) from products";
+                //MySqlCommand sql6 = new MySqlCommand(querymin, connection);
+                //double min = (double)sql6.ExecuteScalar();
+                //Console.WriteLine(min);
+
+                //
                 //creer une requete
-                string query = "SELECT ContactName, CompanyName from customers";
+                string reqSelect = "select ProductName, UnitPrice from products where ProductName like @nomproduit  AND UnitPrice > @prix";
 
-                //Preparer L'execution de la requte
-                MySqlCommand sql = new MySqlCommand(query, connection);
+                //Demander a l'utilisateur la valeur
+                Console.Write("Saisir le nom du produit: ");
+                string nomprd = Console.ReadLine();
+                Console.Write("Saisir le prix min produit: ");
+                double userprix = Double.Parse(Console.ReadLine());
 
-                //Recuperer le cursur
-                MySqlDataReader reader = sql.ExecuteReader();
+                //Definir le parametre utilisé dans l'objet Command
+                MySqlParameter parametre = new MySqlParameter();
+                parametre.ParameterName = "@nomproduit";
+                parametre.Value = "%" + nomprd + "%";
 
-                //Parcourir le curseur
+                MySqlParameter parametre2 = new MySqlParameter();
+                parametre2.ParameterName = "@prix";
+                parametre2.Value = userprix;
+
+                //Definir l'objet command
+                MySqlCommand sql = new MySqlCommand(reqSelect, connection);
+
+                //Ajouter le parametre l'objet Command
+                sql.Parameters.Add(parametre);
+                sql.Parameters.Add(parametre2);
+
+                //Executer la commande
+                reader = sql.ExecuteReader();
+
+                //Afficher les enregistrements
                 while (reader.Read())
                 {
-                    //afficher detail
-                    string nom, company;
-                    nom = (string)reader[0];
-                    company = (string)reader[1];
-                    Console.WriteLine("nom: {0} - compagnie: {1}", nom, company);
+                    string nom = (string)reader[0];
+                    double prix = Convert.ToDouble(reader[1]);
+                    Console.WriteLine("nom produit:{0} - prix: {1:f}", nom, prix);
                 }
+
+
+
+
+
 
             }
             catch (Exception ex)
---
+++ Demo8_ADOMySql/Demo8_SQLite/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using MySql.Data.MySqlClient;
+
+
+namespace Demo8_SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLiteConnection connection = new SQLiteConnection(@"Data Source=G:\ADO\sqlite\sqlitenorthwind.s3db");
+
+            try
+            {
+                Console.WriteLine("Connection sur SQLite...");
+                connection.Open();
+
+                //crrer une requete
+                string query = "SELECT categoryID, description from categories";
+
+                //preparer l'execution de la requete
+                SQLiteCommand sql = new SQLiteCommand(query, connection);
+
+                //Recuperer le curseur
+                SQLiteDataReader reader = sql.ExecuteReader();
+
+                //Parcourir le curseur
+                while (reader.Read())
+                {
+                    //afficher detail
+                    string description;
+                    int id;
+                    id = Convert.ToInt32(reader[0]);
+                    description = (string)reader[1];
+                    Console.WriteLine("categorie: {0} - Description: {1}", id, description);
+                }
+
+  
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
